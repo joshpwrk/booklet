a60aeb0496fc21a3b0a04973ea5760ca24a01da8
Joshua Kim <joshua0702@gmail.com>
2023-03-08 15:50:59 +0900

    feat: add uuid creation

--- websocket-api/orderHandler.js
+++ websocket-api/orderHandler.js
@@ -1,11 +1,22 @@
+import { v4 as uuidv4 } from 'uuid';
 
-async function createOrder(payload) {
-    const socket = this;
+async function createOrder(redisQueue) {
+    return async function (payload) {
+        // Generate a unique UUID for the order
+        const orderId = uuidv4();
+        payload = {...payload, "order_id": orderId}
+
+        console.log(`Received request to create order: ${payload}`);
     
-    // notify the other users
-    socket.broadcast.emit("order:created", "yay");
-
-    console.log(`Created order: ${payload}`);
+        // add the order to a Redis database
+        redisQueue.zadd('queue', Date.now(), payload, (err, result) => {
+            if (err) {
+                console.error(`Error adding order to Redis: ${err}`);
+            } else {
+                console.log(`Added order to Redis: ${result}`);
+            }
+        });
+    }
 }
 
 async function deleteOrder(payload) {
--- websocket-api/server.js
+++ websocket-api/server.js
@@ -2,11 +2,20 @@ import { Server } from "socket.io";
 import { createOrder, deleteOrder } from "./orderHandler.js";
 
 const io = new Server({ /* options */ });
+const redisQueue = redis.createClient({ 
+    host: 'localhost',
+    port: 6379,
+    db: 1 ,
+    retry_strategy: (options) => {
+      console.error(`Redis connection failed. Retrying in ${options.attempt * 1000}ms`);
+      return options.attempt * 1000;
+    }
+});
 
 io.on('connection', (socket) => {
     console.log('A client connected');
 
-    socket.on("order:create", createOrder);
+    socket.on('order:create', createOrder(redisQueue));
     socket.on("order:delete", deleteOrder);
 });
 
